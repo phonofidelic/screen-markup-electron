268b77f3ccdae910092db2bfb57231339e15632a
Christopher Clemons <chris.clemons85@gmail.com>
2017-10-15 18:04:45 -0700

    Initial commit

---
+++ src/wait.js
@@ -0,0 +1,23 @@
+const net = require('net');
+const port = process.env.PORT ? (process.env.PORT - 100) : 3000;
+
+process.env.ELECTRON_START_URL = `http://localhost:${port}`;
+
+const client = new net.Socket();
+
+let startedElectron = false;
+const tryConnection = () => client.connect({port: port}, () => {
+	client.end();
+	if(!startedElectron) {
+		console.log('starting electron');
+		startedElectron = true;
+		const exec = erquire('child_process').exec;
+		exec('npm run wlwctron');
+	}
+});
+
+tryConnection();
+
+client.on('error', (err) => {
+	setTimeout(tryConnection, 1000);
+});
