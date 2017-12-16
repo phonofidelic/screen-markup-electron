b9a155e9389d4972143b33a30de55e0a29a7d4ae
Christopher Clemons <chris.clemons85@gmail.com>
2017-12-16 01:21:36 -0800

    remove unused code

--- src/containers/ToolsContainer.js
+++ src/containers/ToolsContainer.js
@@ -54,36 +54,6 @@ class ToolsContainer extends Component {
                     className="tool"
                     onClick={() => { toggleTools() }} />
         {tools.showTools && this.renderTools()}
-
-        {/*<FontAwesome name='crop' 
-                             className="tool" 
-                             title="Crop image"
-                             style={tools.selectedTool.type === 'crop' ? {background: '#515151'} : null}
-                             onClick={() => { selectCrop() }} />*/}
-
-        {/*<FontAwesome name='square' 
-                             className="tool color-picker" 
-                             title="Color" 
-                             style={{color: tools.selectedColor}}/>*/}
-
-
-
-        {/*<FontAwesome name='eraser' 
-                             className="tool" 
-                             title="Erase"
-                             style={tools.selectedTool.type === 'eraser' ? {background: '#515151'} : null}
-                             onClick={() => { selectEraser() }} />*/}
-
-        {/*<FontAwesome name='font' 
-                             className="tool" 
-                             title="Text"
-                             style={tools.selectedTool.type === 'text' ? {background: '#515151'} : null}
-                             onClick={() => { selectText() }} />*/}
-
-        
-
-
-        {/*<FontAwesome name='times' className="tool" title="Cancel" />*/}
       </div>
 		);
 	}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import toolsReducer from './toolsReducer';
-import canvasReducer from './canvasReducer';
+// import canvasReducer from './canvasReducer';
 
 const rootReducer = combineReducers({
 	toolsReducer: toolsReducer,
-	canvasReducer: canvasReducer
+	// canvasReducer: canvasReducer
 })
 
 export default rootReducer;
